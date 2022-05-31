d8a3780ed944ab59b6db76df0ac66fc809c54afc
Collins <mutaic84@gmail.com>
2022-05-31 13:10:41 +0300

    basic data type declaration and usage

---
+++ hello.rb
@@ -0,0 +1,8 @@
+character_name = "Samuel"
+character_age = 35
+
+puts ("There was once a man named " + character_name)
+puts ("he was " + character_age.to_s + " years old.")
+character_name = "Tom"
+puts ("He really liked the name " + character_name)
+puts ("but didn't like being " + character_age.to_s)
