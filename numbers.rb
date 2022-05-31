a334537b70d1e0ad703fe0b94930ccd048ed59c9
Collins <mutaic84@gmail.com>
2022-05-31 13:53:54 +0300

    basics of working with numbers in ruby

---
+++ numbers.rb
@@ -0,0 +1,27 @@
+# working with numbers in ruby
+
+num = 20.5667
+
+puts -6.989 + 10
+
+# exponenials
+puts 2**3
+
+# modulus
+puts 10 % 3
+
+puts ("My fav num is: " + num.to_s)
+
+# methods used with numbers
+# get the absolute value of a number
+puts num.abs()
+
+# with decimals
+# puts num.round()
+# puts num.ceil()
+# puts num.floor()
+
+puts Math.log(1)
+
+# working with floating point numbers and integer numbers
+puts 10/7.0
