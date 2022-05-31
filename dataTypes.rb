e500bb14c6b9b53173568f3c3d0cd432a570e51c
Collins <mutaic84@gmail.com>
2022-05-31 13:21:03 +0300

    basic available data types in ruby

---
+++ dataTypes.rb
@@ -0,0 +1,13 @@
+# string
+name = "Ckm"
+occupation = "developer"
+# number
+# integer
+age = 75
+# decimal --- floating point
+gpa = 3.286
+# boolean i.e true or false data
+ismale = true
+istall = false
+# nil
+flaws = nil
