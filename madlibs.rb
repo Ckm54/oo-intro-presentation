097e86571d53262902dbeb61595829a22d94558b
Collins <mutaic84@gmail.com>
2022-06-01 22:57:16 +0300

    create a simple program --- a madlibs game

---
+++ madlibs.rb
@@ -0,0 +1,13 @@
+# create a simple madlibs game -- practice gathering inputs
+
+puts "Enter a color: "
+color = gets.chomp()
+puts "Enter a plural noun: "
+plural_noun = gets.chomp()
+puts "Enter a celebrity: "
+celebrity = gets.chomp()
+
+
+puts ("Roses are " + color)
+puts (plural_noun + " are blue")
+puts ("I love " + celebrity)
