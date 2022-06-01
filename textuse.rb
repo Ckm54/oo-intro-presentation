4452270c2f7614fa832c855b049d2ee07c3a0ac3
Collins <mutaic84@gmail.com>
2022-06-01 22:51:45 +0300

    create a basic calculator to add two numbers --- working with numnbers as inputs

--- textuse.rb
+++ textuse.rb
@@ -1,10 +1,21 @@
-# allow user to input some information and store it in a variable
-puts "Enter your name: "
+# # allow user to input some information and store it in a variable
+# puts "Enter your name: "
 
-# use chomp to get rid of a new line when a user inputs some value tto the program
-name = gets.chomp()
+# # use chomp to get rid of a new line when a user inputs some value tto the program
+# name = gets.chomp()
 
-puts "Enter your age: "
-age = gets.chomp()
+# puts "Enter your age: "
+# age = gets.chomp()
 
-puts ("Hello " + name + ", you are " + age)
+# puts ("Hello " + name + ", you are " + age
+
+
+# GETTING NUMBERS ---- SIMPLE CALCULATOR
+puts "Enter a number: "
+num1 = gets.chomp().to_f
+puts "Enter another number: "
+num2 = gets.chomp().to_f
+
+
+# convert input numbers to integers
+puts (num1 + num2)
