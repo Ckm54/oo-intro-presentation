641e145206bab0f8ad271af68a584f3336999e08
Collins <mutaic84@gmail.com>
2022-06-03 13:37:05 +0300

    use comparison operators to create conditionals

--- loops.rb
+++ loops.rb
@@ -1,12 +1,27 @@
-ismale = false
-istall = false
+# ismale = false
+# istall = false
 
-if ismale and istall
-    puts "You are a tall male"
-elsif ismale and !istall
-    puts "You are a short male"
-elsif !ismale and istall
-    puts "You are not male and tall"
-else
-    puts "You are not male and not tall"
+# if ismale and istall
+#     puts "You are a tall male"
+# elsif ismale and !istall
+#     puts "You are a short male"
+# elsif !ismale and istall
+#     puts "You are not male and tall"
+# else
+#     puts "You are not male and not tall"
+# end
+
+
+#  using comparisons wit a conditional
+
+def max(num1,num2,num3)
+    if num1 >= num2 and num1 >= num3
+        return num1
+    elsif num2 >= num1 and num2 >= num3
+        return num2
+    else
+        return num3
+    end
 end
+
+puts max(24,200,113)
