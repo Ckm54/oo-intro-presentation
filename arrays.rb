f30661db60d8be94526b165246e3c6dbe19b7e2f
Collins <mutaic84@gmail.com>
2022-06-03 12:32:29 +0300

    working with arrays

--- arrays.rb
+++ arrays.rb
@@ -1,6 +1,12 @@
 # dealing with arrays to store and keep track of a number of variables
 friends = Array["Fred", "Kevin", "Karen", "Oscar"]
 
-
+# print first element of the array
+puts friends[0]
 puts friends.include? "Karen"
+
+# reverse order of the array
 puts friends.reverse()
+
+# get a range of characters from the array
+puts friends[1, 2]
