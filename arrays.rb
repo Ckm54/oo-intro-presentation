5f86faa33eb97e2d7d1bf832bb8d8a3fee62538a
Collins <mutaic84@gmail.com>
2022-06-03 12:34:57 +0300

    arrays concluded

--- arrays.rb
+++ arrays.rb
@@ -3,6 +3,8 @@ friends = Array["Fred", "Kevin", "Karen", "Oscar"]
 
 # print first element of the array
 puts friends[0]
+
+# check if an element is in an array
 puts friends.include? "Karen"
 
 # reverse order of the array
@@ -10,3 +12,6 @@ puts friends.reverse()
 
 # get a range of characters from the array
 puts friends[1, 2]
+
+# sort elements in an array
+puts friends.sort()
