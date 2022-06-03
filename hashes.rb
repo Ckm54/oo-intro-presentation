170fe3bfe534839eba100708a0654fc230b38129
Collins <mutaic84@gmail.com>
2022-06-03 12:54:03 +0300

    hashes

--- hashes.rb
+++ hashes.rb
@@ -1,1 +1,8 @@
-# Enables storage of a key value pair
+# Enables storage of a key value pair with keys being unique
+states = {
+    "Pennysylvania" => "PA",
+    "New York" => "NY",
+    "Oregon" => "OR"
+}
+
+puts states["Pennysylvania"]
