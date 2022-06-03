9022625f5dac6a1d1f24ee7f8c06da0114435756
Collins <mutaic84@gmail.com>
2022-06-03 22:45:03 +0300

    create a simple quiz game using ruby

---
+++ quiz.rb
@@ -0,0 +1,4 @@
+# create a multiple choice quiz using ruby
+p1 = "What color are apples?\n(a)red\n(b)purple\n(c)orange"
+p2 = "What color are bananas?\n(a)pink\n(b)red\n(c)yellow"
+p3 = "What color are pears?\n(a)yellow\n(b)green\n(c)orange"
