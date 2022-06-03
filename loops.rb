dbc5e931b77892bcca4fcd67ed701c983c2bb15c
Collins <mutaic84@gmail.com>
2022-06-03 18:46:45 +0300

    different ways of looping in ruby

--- loops.rb
+++ loops.rb
@@ -14,14 +14,30 @@
 
 #  using comparisons wit a conditional
 
-def max(num1,num2,num3)
-    if num1 >= num2 and num1 >= num3
-        return num1
-    elsif num2 >= num1 and num2 >= num3
-        return num2
-    else
-        return num3
-    end
+# def max(num1,num2,num3)
+#     if num1 >= num2 and num1 >= num3
+#         return num1
+#     elsif num2 >= num1 and num2 >= num3
+#         return num2
+#     else
+#         return num3
+#     end
+# end
+
+# puts max(24,200,113)
+
+
+# FOR LOOPS
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+# friends.each do |friend|
+#     puts friend
+# end
+
+# loop through a range of numbers
+for index in 0..5
+    puts index
 end
 
-puts max(24,200,113)
+6.times do |index|
+    puts index
+end
