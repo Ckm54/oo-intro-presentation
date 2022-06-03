528ca11cf3a27cc3edc1675d0178508c8f07ff6a
Collins <mutaic84@gmail.com>
2022-06-03 13:06:22 +0300

    add working with methods

--- hashes.rb
+++ hashes.rb
@@ -1,8 +1,15 @@
 # Enables storage of a key value pair with keys being unique
-states = {
-    "Pennysylvania" => "PA",
-    "New York" => "NY",
-    "Oregon" => "OR"
-}
+# states = {
+#     "Pennysylvania" => "PA",
+#     "New York" => "NY",
+#     "Oregon" => "OR"
+# }
 
-puts states["Pennysylvania"]
+# puts states["Pennysylvania"]
+
+# Methods
+def say_hello(name="no name", age=-1)
+    puts ("Hello " + name + " You are " + age.to_s)
+end
+
+say_hello()
