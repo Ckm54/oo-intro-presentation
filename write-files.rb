dc84faad3d3d63168afe4db34bd9d5b0cb279e1c
Collins <mutaic84@gmail.com>
2022-06-03 19:28:11 +0300

    working with files ---reading and writing

--- write-files.rb
+++ write-files.rb
@@ -1,3 +1,8 @@
-File.open("files/employees.txt", "r") do |file|
-    
+# File.open("files/index.html", "w") do |file|
+#     file.write("<h1>Hello</h1>")
+# end
+
+File.open("files/employees.txt", "r+") do |file|
+    file.readchar()
+    file.write("Hi")
 end
